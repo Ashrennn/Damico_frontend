4bf0722df1b4af7290a0fd9a627e282622c70549
Clarhence Oliveros <134087492+Ashrennn@users.noreply.github.com>
2025-07-30 21:32:07 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderbarComponent } from './components/headerbar/headerbar.component';
-import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, HeaderbarComponent, HomeComponent, FormsModule],
+  imports: [RouterOutlet, NavbarComponent, HeaderbarComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
